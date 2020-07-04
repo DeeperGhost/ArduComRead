dbb2536d6bbc50f959ca6ad918d37c9c37690d59
009 <kareolis@gmail.com>
2020-07-05 00:52:11 +1000

    передача пути для сохранния последних данных через параметр функции

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import serial
 import datetime
 
-def addData(h,t):
+def addData(h,t,PathFile):
     dt = datetime.datetime.now()
 
     f = open("data/" + dt.strftime("%Y-%m-%d") + '.csv', 'a')
     f.write(str(h)+";"+str(t)+";"+str(dt)+"\n")
     f.close()
 
-    # f = open("C:/Users/007/PycharmProjects/FlaskServTest/data/indicators.i",'w')
+    # f = open(PathFile,'w')
     # f.write(str(h) + ";" + str(t) + ";" + str(dt) + "\n")
     # f.close()
 
 def ON():
     # comport = 'COM3' # for win
     comport = '/dev/ttyACM0'
+    fileForReadNow ='C:/Users/007/PycharmProjects/FlaskServTest/data/indicators.i'
     try:
         port = serial.Serial(comport,9600)
         while 1:
             buf = str(port.readline())
-            addData(h=int(buf[3:5]), t=int(buf[7:9]))
+            addData(h=int(buf[3:5]), t=int(buf[7:9]), PathFile=fileForReadNow)
             print(buf[2:9])
         port.close()
 
